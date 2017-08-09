b323600c6fdcc1ee1e15160c7bf06b87b3faca67
mehdi <mehdi@cefalo.com>
2017-08-09 11:41:36 +0600

    Initial Commit

---
+++ YouTubePlayer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  YouTubePlayer
+//
+//  Created by Mehdi Hasan on 8/9/17.
+//  Copyright © 2017 Mehdi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
